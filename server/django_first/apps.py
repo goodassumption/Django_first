358e6ee156871fba3562d99acc08a5146870d374
goodassumption <alexgolovin2010@yandex.ru>
2025-12-16 20:18:30 +0300

    Django 0.2

---
+++ server/django_first/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoFirstConfig(AppConfig):
+    name = 'django_first'
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djando_first',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['django_first/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
