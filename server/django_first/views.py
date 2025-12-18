766c57ad43ed09a390e9784ae3d8820c2f4e972f
GoodAssumption <alex.golovin.2010@gmail.com>
2025-12-18 15:03:27 +0300

    Django 0.4

--- server/django_first/views.py
+++ server/django_first/views.py
@@ -14,12 +14,36 @@ def time(request):
     context = {}
     return render(request, 'time.html', context)
 
+def calc(request):
+    context = {}
+    if request.method == 'POST':
+        action = request.POST.get('action', 'add')
+        n1 = float(request.POST.get('first'))
+        n2 = float(request.POST.get('second'))
+        ans = 0.0
+
+        match action:
+            case 'sum':
+                ans = n1 + n2
+                context['ans'] = f'{n1} + {n2} = {str(ans)}'
+            case 'sub':
+                ans = n1 - n2
+                context['ans'] = f'{n1} - {n2} = {str(ans)}'
+            case 'mul':
+                ans = n1 * n2
+                context['ans'] = f'{n1} * {n2} = {str(ans)}'
+            case 'div':
+                if n2 == 0:
+                    context['ans'] = 'На ноль делить нельзя!'
+                    return render(request, 'calc.html', context)
+                ans = n1 / n2
+                context['ans'] = f'{n1} / {n2} = {str(ans)}'
+
+    context['text'] = 'Итоговая операция:'
+    return render(request, 'calc.html', context)
+
 def time_update(request):
     context = {}
     now = datetime.datetime.now()
     context['date'], context['time'] = now.strftime("%Y-%m-%d %H:%M:%S").split()
     return JsonResponse(context)
-
-def calc(request):
-    context = {}
-    return render(request, 'calc.html', context)
