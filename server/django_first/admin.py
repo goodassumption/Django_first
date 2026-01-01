58bcd1dc86cf4abc50ac7d187cb34f6c689748e6
GoodAssumption <alex.golovin.2010@gmail.com>
2026-01-01 21:35:03 +0300

    add: added models for reviews and characters. Happy New Year!

--- server/django_first/admin.py
+++ server/django_first/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import CalcHistory
+from .models import *
 
 @admin.register(CalcHistory)
 class CalcHistoryAdmin(admin.ModelAdmin):
-    list_display = ('expression', 'result', 'time')  # что показывать в списке
-    list_filter = ('time',)  # фильтры справа
-    search_fields = ('expression',)  # поиск по выражению
+    list_display = ('expression', 'result', 'time')
+    list_filter = ('time',)
+    search_fields = ('expression',)
+
+@admin.register(CharactersModel)
+class CharactersModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'time')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+@admin.register(ReviewModel)
+class ReviewModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'time')
+    search_fields = ('name',)
+    list_filter = ('name',)
--- server/django_first/models.py
+++ server/django_first/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from datetime import datetime
+
+def create_time():
+    now = datetime.now()
+    return now.strftime("%d-%m-%Y %H:%M:%S")
 
 class CalcHistory(models.Model):
     expression = models.CharField(max_length=100)
@@ -14,3 +19,28 @@ class CalcHistory(models.Model):
         verbose_name = "История вычислений"
         verbose_name_plural = "История вычислений"
         ordering = ['-time']  # сортировка по убыванию времени
+
+class CharactersModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя персонажа')
+    url = models.CharField(max_length=100, verbose_name='Ссылка на картинку')
+    time = models.CharField(max_length=50, default=create_time(), editable=False, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Персонаж"
+        verbose_name_plural = "Персонажи"
+
+class ReviewModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название фильма')
+    review = models.TextField(verbose_name='Рецензия')
+    url = models.CharField(max_length=100, default='', verbose_name='Ссылка на постер')
+    time = models.CharField(max_length=50, default=create_time(), editable=False, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Рецензия"
+        verbose_name_plural = "Рецензии"    
